fac06ff8973c372667bf3df11d32b03637d1e7dc
Shahriarhossain123 <9763691+Shahriarhossain123@users.noreply.github.com>
2021-10-23 00:28:16 +0600

    utils 2

--- car_number_plate/forms.py
+++ car_number_plate/forms.py
@@ -1,4 +1,7 @@
+import os.path
 from django import forms
+from django.contrib.admin.helpers import ActionForm
+from django.utils.translation import gettext_lazy as _
 from .models import Car, Driver
 
 
@@ -12,3 +15,61 @@ class DriverForm(forms.ModelForm):
     class Meta:
         model = Driver
         fields = ['name', 'phone', 'address', 'car', 'photo', 'entry_time']
+
+
+class ImportForm(forms.Form):
+    import_file = forms.FileField(
+        label=_('File to import')
+    )
+    input_format = forms.ChoiceField(
+        label=_('Format'),
+        choices=(),
+    )
+
+    def __init__(self, import_formats, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        choices = []
+        for i, f in enumerate(import_formats):
+            choices.append((str(i), f().get_title(),))
+        if len(import_formats) > 1:
+            choices.insert(0, ('', '---'))
+
+        self.fields['input_format'].choices = choices
+
+
+class ConfirmImportForm(forms.Form):
+    import_file_name = forms.CharField(widget=forms.HiddenInput())
+    original_file_name = forms.CharField(widget=forms.HiddenInput())
+    input_format = forms.CharField(widget=forms.HiddenInput())
+
+    def clean_import_file_name(self):
+        data = self.cleaned_data['import_file_name']
+        data = os.path.basename(data)
+        return data
+
+
+class ExportForm(forms.Form):
+    file_format = forms.ChoiceField(
+        label=_('Format'),
+        choices=(),
+    )
+
+    def __init__(self, formats, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        choices = []
+        for i, f in enumerate(formats):
+            choices.append((str(i), f().get_title(),))
+        if len(formats) > 1:
+            choices.insert(0, ('', '---'))
+
+        self.fields['file_format'].choices = choices
+
+
+def export_action_form_factory(formats):
+    class _ExportActionForm(ActionForm):
+        file_format = forms.ChoiceField(
+            label=_('Format'), choices=formats, required=False)
+
+    _ExportActionForm.__name__ = str('ExportActionForm')
+
+    return _ExportActionForm
